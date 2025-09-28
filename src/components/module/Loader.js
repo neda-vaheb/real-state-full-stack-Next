80fe69d12328a4dbddb4e23534226311b041b0ab
Neda Vaheb <n.vaheb.nv@gmail.com>
2025-09-28 05:21:25 -0700

    fix :remove spinner

--- src/components/module/Loader.js
+++ src/components/module/Loader.js
@@ -1,17 +1,8 @@
-import { ThreeDots } from "react-loader-spinner";
 
 function Loader() {
   return (
-    <ThreeDots
-      visible={true}
-      height="45"
-      width="45"
-      color="primary"
-      radius="9"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{ margin: "auto" }}
-      wrapperClass=""
-    />
+    <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-blue-500"></div>
+
   );
 }
 
